6fdf23ea86c0766fee25f9acc88d87fe1cd6e452
Rob-Clancy-Git-Hub <53898091+Rob-Clancy-Git-Hub@users.noreply.github.com>
2023-12-18 16:00:59 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-resource "aws_s3 _bucket" "test_bucket" { 
+resource aws_s3 _bucket test_bucket { 
 bucket = "rob-my-tf-test-bucket"
 acl = "private"
 
 tags = {
 Name = "My bucket"
-git_commit "bb71a5b5027a9c7da866c f6697aec207b570f5a"
-git_file "main.tf"
+git_commit = "bb71a5b5027a9c7da866c f6697aec207b570f5a"
+git_file = "main.tf"
 git_last_modified_at = "2021-07-22 06:54:36"
 git_last_modified_by = "rob@users.noreply.github.com"
-git_repo "drift-2"
-yor_trace "rob-drift-yor-tag"
+git_repo = "drift-2"
+yor_trace = "rob-drift-yor-tag"
     }
 }
